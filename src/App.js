f09857cf18db5d107b967c8b6ff4d3453355ce2b
Kortney <kortney@iMac.local>
2022-07-06 16:31:29 -0300

    added a delete and wrote comments to explain what all the pieces do

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import { useState, useEffect } from "react";
 
 function App() {
     const [pokemonCards, setPokemonCards] = useState([]);
+    const [name, setName] = useState("");
+    const [type, setType] = useState("");
+    const [HP, setHP] = useState("");
+    const [ID, setID] = useState("");
+
+    //because there is nothing in dependency array, runs one time when you load the page
+    useEffect(() => {
+        refreshPokemonCards();
+    }, []);
+
+    //runs a fetch that gets all the pokemon and sets them into variable. Its a function so we can call it later when needed
     const refreshPokemonCards = () => {
         fetch("http://localhost:8000/api/pokemoncards")
             .then((response) => response.json())
@@ -10,14 +21,69 @@ function App() {
             });
     };
 
-    useEffect(() => {
-        refreshPokemonCards();
-    }, []);
+    // creates an object that is the same shape as what the API can accept and then passes it into the API POST body. Runs refreshPokemonCards to give us back new data that was updated
+    function save(event) {
+        event.preventDefault();
+
+        let pokemonCard = {
+            name: name,
+            pokemonType: type,
+            HP: HP,
+        };
+
+        fetch("http://localhost:8000/api/pokemoncards/", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(pokemonCard),
+        }).then(() => refreshPokemonCards());
+    }
+
+    function deletePokemon(event) {
+        event.preventDefault();
+
+        fetch(`http://localhost:8000/api/pokemoncards/${ID}`, {
+            method: "DELETE",
+        }).then(() => refreshPokemonCards());
+    }
+
     return (
         <div className="App">
             {pokemonCards.map((card, i) => (
                 <p key={i}>{card.name}</p>
             ))}
+            <form>
+                <h2>Add a Pokemon Card</h2>
+                <input
+                    onChange={(event) => setName(event.target.value)}
+                    type="text"
+                    placeholder="Name"
+                ></input>
+
+                <input
+                    onChange={(event) => setType(event.target.value)}
+                    type="text"
+                    placeholder="Type"
+                ></input>
+                <input
+                    onChange={(event) => setHP(event.target.value)}
+                    type="text"
+                    placeholder="HP"
+                ></input>
+                <button onClick={save}>Submit</button>
+            </form>
+
+            <form>
+                <h2>Delete a Pokemon Card</h2>
+                <input
+                    onChange={(event) => setID(event.target.value)}
+                    type="text"
+                    placeholder="ID"
+                ></input>
+                <button onClick={deletePokemon}>Delete</button>
+            </form>
         </div>
     );
 }
