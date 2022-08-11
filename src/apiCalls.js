17c424cc15d4db5d3e97eec774badc2aea2bfaf5
Kortney <kortney@iMac.local>
2022-08-11 11:43:19 -0300

    made a seperate file for an axios instance with our API, so we can use it to make a call for our API server address

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,27 +1,23 @@
-import axios from "axios";
+import API from "./api";
 
 export function getAllPokemonCards(setPokemonCards) {
-    axios
-        .get("http://localhost:8000/api/pokemoncards")
-        .then((response) => setPokemonCards(response.data));
+    API.get("pokemoncards/").then((response) => setPokemonCards(response.data));
 }
 
 export function getAllPokemonCollections(setCollectedArray) {
-    axios
-        .get("http://localhost:8000/api/pokemoncollections/")
-        .then((response) => setCollectedArray(response.data));
+    API.get("pokemoncollections/").then((response) =>
+        setCollectedArray(response.data)
+    );
 }
 
 export function getAllPokemonTypes(setPokemonTypes) {
-    axios
-        .get("http://localhost:8000/api/pokemontypes/")
-        .then((response) => setPokemonTypes(response.data));
+    API.get("pokemontypes/").then((response) => setPokemonTypes(response.data));
 }
 
 export function getAllPokemonSets(setPokemonCardSets) {
-    axios
-        .get("http://localhost:8000/api/pokemoncardsets")
-        .then((response) => setPokemonCardSets(response.data));
+    API.get("pokemoncardsets/").then((response) =>
+        setPokemonCardSets(response.data)
+    );
 }
 
 export function putSelectedCard(
@@ -29,18 +25,15 @@ export function putSelectedCard(
     selectedPokemon,
     refreshPokemonCards
 ) {
-    axios
-        .put(
-            `http://localhost:8000/api/pokemoncards/${selectedPokemon.id}/`,
-            pokemonCardData
-        )
-        .then(() => refreshPokemonCards());
+    API.put(`pokemoncards/${selectedPokemon.id}/`, pokemonCardData).then(() =>
+        refreshPokemonCards()
+    );
 }
 
 export function createNewPokemonCard(pokemonCardData, refreshPokemonCards) {
-    axios
-        .post("http://localhost:8000/api/pokemoncards/", pokemonCardData)
-        .then(() => refreshPokemonCards());
+    API.post("pokemoncards/", pokemonCardData).then(() =>
+        refreshPokemonCards()
+    );
 }
 
 export function updateSelectedCollection(
@@ -48,34 +41,29 @@ export function updateSelectedCollection(
     collectionObj,
     refreshPokemonCollections
 ) {
-    axios
-        .put(
-            `http://localhost:8000/api/pokemoncollections/${item.id}/`,
-            collectionObj
-        )
-        .then(() => refreshPokemonCollections());
+    API.put(`pokemoncollections/${item.id}/`, collectionObj).then(() =>
+        refreshPokemonCollections()
+    );
 }
 
 export function createNewCollection(collectionObj, refreshPokemonCollections) {
-    axios
-        .post("http://localhost:8000/api/pokemoncollections/", collectionObj)
-        .then(() => refreshPokemonCollections());
+    API.post("pokemoncollections/", collectionObj).then(() =>
+        refreshPokemonCollections()
+    );
 }
 
 export function deleteSelectedCollection(card, refreshPokemonCollections) {
-    axios
-        .delete(`http://localhost:8000/api/pokemoncollections/${card.id}`)
-        .then(() => refreshPokemonCollections());
+    API.delete(`pokemoncollections/${card.id}`).then(() =>
+        refreshPokemonCollections()
+    );
 }
 
 export function deleteSelectedCard(cardID, refreshPokemonCards) {
-    axios
-        .delete(`http://localhost:8000/api/pokemoncards/${cardID}`)
-        .then(() => refreshPokemonCards());
+    API.delete(`pokemoncards/${cardID}`).then(() => refreshPokemonCards());
 }
 
 export function getFilteredPokemon(typeFilter, setPokemonCards) {
-    axios
-        .get(`http://localhost:8000/api/pokemoncards?pokemontype=${typeFilter}`)
-        .then((response) => setPokemonCards(response.data));
+    API.get(`pokemoncards?pokemontype=${typeFilter}`).then((response) =>
+        setPokemonCards(response.data)
+    );
 }
