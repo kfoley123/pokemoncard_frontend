e237e6683b4733d8149308c9cf1c8bb8dc26c37c
Kortney <kortney@iMac.local>
2022-09-29 14:09:14 -0300

    added the link to the live api endpoint

--- src/Helpers/api.js
+++ src/Helpers/api.js
@@ -1,3 +1,5 @@
 import axios from "axios";
 
-export default axios.create({ baseURL: "http://localhost:8000/api/" });
+export default axios.create({
+    baseURL: "https://dry-tor-16883.herokuapp.com/api/",
+});
