5ee54a9d3469ad8ac7d47b4a09ded7f3b713af0e
Kortney <kortney@iMac.local>
2022-10-05 15:32:41 -0300

    fixed line spacing error on collections page and updated API endpoint for Live version of app

--- src/Helpers/api.js
+++ src/Helpers/api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8000/api/",
-    // baseURL: "https://dry-tor-16883.herokuapp.com/api/",
+    baseURL: "https://dry-tor-16883.herokuapp.com/api/",
 });
